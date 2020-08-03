07cdabca130b17d95ba0b2fa22185d56218453af
snep <snep@snep.pw>
2020-08-03 15:00:20 -0700

    more card list filters + shio prep

--- default/static/js/card_list.js
+++ default/static/js/card_list.js
@@ -1,11 +1,15 @@
 window.memberFilterSelector = null;
 window.rarityFilterSelector = null;
+window.attributeFilterSelector = null;
+window.roleFilterSelector = null;
 
 function updateFilter() {
     var mf = window.memberFilterSelector || '';
     var rf = window.rarityFilterSelector || '';
+    var af = window.attributeFilterSelector || '';
+    var tf = window.roleFilterSelector || '';
 
-    var select = '' + mf + rf;
+    var select = '' + mf + rf + af + tf;
 
     if (select.length > 0) {
         $('.filtered-card').hide();
@@ -41,6 +45,36 @@ $('.rarity-filter').click(function(e) {
         $(this).addClass('img-hl');
     } else {
         window.rarityFilterSelector = null;
+    }
+    updateFilter();
+});
+
+$('.attribute-filter').click(function(e) {
+    e.preventDefault();
+    var attrib = $(this).attr('attribute');
+    var as = '.card-attribute-' + attrib;
+
+    $('.attribute-filter.img-hl').removeClass('img-hl');
+    if (window.attributeFilterSelector !== as) {
+        window.attributeFilterSelector = as;
+        $(this).addClass('img-hl');
+    } else {
+        window.attributeFilterSelector = null;
+    }
+    updateFilter();
+});
+
+$('.role-filter').click(function(e) {
+    e.preventDefault();
+    var attrib = $(this).attr('role');
+    var ts = '.card-role-' + attrib;
+
+    $('.role-filter.img-hl').removeClass('img-hl');
+    if (window.roleFilterSelector !== ts) {
+        window.roleFilterSelector = ts;
+        $(this).addClass('img-hl');
+    } else {
+        window.roleFilterSelector = null;
     }
     updateFilter();
 });
