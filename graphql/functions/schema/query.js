754f7c024a91e6e88a300dfd72336282ca5c46fc
snep <snep@snep.pw>
2020-12-06 02:48:29 -0800

    fix card id search

--- graphql/functions/schema/query.js
+++ graphql/functions/schema/query.js
@@ -2,7 +2,7 @@ const { gql } = require("apollo-server-cloud-functions");
 
 exports.typeDefs = gql`
     type Query {
-        card(id: String, no: Int): Card
+        card(id: Int, no: Int): Card
         cards: [Card]
     }
 `;
@@ -12,7 +12,7 @@ exports.resolvers = {
         card: async (parent, args, context) => {
             const cardData = await context.dataSources.data.getObject("cards");
             if (args.id) {
-                return cardData[args.id];
+                return cardData[args.id + ""];
             } else if (args.no) {
                 return Object.values(cardData).find((c) => c.no == args.no);
             }
